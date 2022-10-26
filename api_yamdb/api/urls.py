9ffc19d843652b1ac1dab7e5f2621e3b421f95ef
Timofey Kondakov <tkondakov98@yandex.ru>
2022-10-26 22:08:13 +0500

    наброски вьюсетов и эндпоинтов

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -5,13 +5,13 @@ from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
                     ReviewViewSet, TitleViewSet)
 
 router = DefaultRouter()
-router.register(r'title', TitleViewSet, basename='title')
-router.register(r'category', CategoryViewSet, basename='category')
+router.register(r'titles', TitleViewSet, basename='title')
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'titles/(?P<title>\d+)/reviews', ReviewViewSet, basename='review')
+router.register(r'genres', GenreViewSet, basename='genre')
 router.register(
-    r'review/(?P<review_id>\d+)/comments', CommentViewSet, basename='comment'
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments', CommentViewSet, basename='comment'
 )
-router.register(r'genre', GenreViewSet, basename='genre')
-router.register(r'review', ReviewViewSet, basename='review')
 
 urlpatterns = [
     path('v1/', include(router.urls))
